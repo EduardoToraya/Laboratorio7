3615e1d0d3fb10dd1ddbc0d34c470d17d7131427
Eduardo Alejandro Toraya Solís <eduar_ts1997@hotmail.com>
2019-11-18 22:56:33 -0600

    upd2

--- str/db/db.js
+++ str/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const connectionURL = "mongodb+srv://edu97:<password>@cluster0-spf7p.mongodb.net/test?retryWrites=true&w=majority"
+const connectionURL = "mongodb+srv://edu97:edu97@cluster0-spf7p.mongodb.net/test?retryWrites=true&w=majority"
 
 mongoose.connect(connectionURL, {
   uneNewUrlParser: true,
