0968fa0ffc859aaa9c8740d196dd8df78ac6bd8a
Eduardo Alejandro Toraya Solís <eduar_ts1997@hotmail.com>
2019-11-18 23:13:47 -0600

    renew

--- str/db/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-const connectionURL = "mongodb+srv://edu97:edu97@cluster0-spf7p.mongodb.net/test?retryWrites=true&w=majority"
-
-mongoose.connect(connectionURL, {
-  uneNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-})
